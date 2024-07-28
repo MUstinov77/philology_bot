57d328d5747ff3fff1ee1c2294def7e784c8e2a7
Max Ustinov <m.ustinov2001@yandex.ru>
2024-07-28 22:08:55 +0300

    web version

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram import Bot, Dispatcher
+from aiohttp import web
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+import logging
 
-import asyncio
 
 from handlers import start_messages, test_messages, accent_test
 from config import config
@@ -14,20 +16,39 @@ routers = (
     test_messages.router,
     accent_test.router
 )
+BOT_TOKEN = config.bot_token.get_secret_value()
+WEBHOOK_HOST = 'https://filology-bot-6.onrender.com'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+bot = Bot(token=BOT_TOKEN,
+          default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+          )
+
+dp = Dispatcher(bot=bot, storage=MemoryStorage())
+dp.include_routers(*routers)
 
 
-async def main():
-    bot = Bot(token=config.bot_token.get_secret_value(),
-              default=DefaultBotProperties(
-                  parse_mode=ParseMode.HTML
-              )
-              )
-
-    dp = Dispatcher(storage=MemoryStorage())
-    dp.include_routers(*routers)
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+async def on_startup(bot: Bot) -> None:
+    await bot.set_webhook(url=WEBHOOK_URL)
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+
+
+def main():
+    dp.startup.register(on_startup)
+    app = web.Application()
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+    )
+    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+    setup_application(app, dp, bot=bot)
+    web.run_app(app, host='0.0.0.0', port=10000)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
+
