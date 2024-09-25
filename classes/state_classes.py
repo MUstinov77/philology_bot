e4fbf83e8f41c8d2a402f8d9513f87afb9ebb970
Max Ustinov <m.ustinov2001@yandex.ru>
2024-09-25 14:07:47 +0300

    stats added

--- classes/state_classes.py
+++ classes/state_classes.py
@@ -11,6 +11,12 @@ class Test(StatesGroup):
 class Statistics:
     all_questions = 0
     right_answers_count = 0
-    questions_and_answers_list: list[tuple] = []
+    #questions_and_answers_list: list[tuple] = []
 
+    def count_right_percent(self):
+        return round(self.right_answers_count / self.all_questions * 100, 1)
+
+    def clear_stats(self):
+        self.all_questions = 0
+        self.right_answers_count = 0
 
--- handlers/accent_test.py
+++ handlers/accent_test.py
@@ -44,7 +44,8 @@ async def start_test(message: Message, state: FSMContext):
     await message.answer('Придумываю тест...\n'
                          'Выберите вариант, в котором правильно поставлено ударение\n'
                          'Учитывайте контекст, в круглых скобках\n'
-                         'Напишите <b>Отмена</b>, чтобы вернуться к выбору теста')
+                         'Напишите <b>СТОП</b>, чтобы узнать свои результаты и вернуться к выбору теста'
+                         )
     await message.answer(accent_test.question,
                          reply_markup=keyboards.KEYBOARD_ANSWERS)
 
@@ -52,11 +53,12 @@ async def start_test(message: Message, state: FSMContext):
 @router.message(accent_test.test_in_progress, F.text.isdigit())
 async def process_answer(message: Message):
     if message.text == accent_test.right_answer:
+        user_statistics.right_answers_count += 1
+        user_statistics.all_questions += 1
         await message.answer('Правильно!')
     else:
-        await message.answer('Неправильно...\n'
-                             f'Правильный ответ - {accent_test.right_answer}'
-                             )
+        user_statistics.all_questions += 1
+        await message.answer('Неправильно...')
     query = cur.execute(f'''
         SELECT question, right_answer 
         FROM questions
@@ -71,8 +73,15 @@ async def process_answer(message: Message):
     await message.answer(accent_test.question, reply_markup=keyboards.KEYBOARD_ANSWERS)
 
 
-@router.message(accent_test.test_in_progress, F.text.lower() == 'отмена')
+@router.message(accent_test.test_in_progress, F.text.lower() == 'стоп')
 async def cancel_test(message: Message, state: FSMContext):
+    user_right_answers_percent = user_statistics.count_right_percent()
+    await message.answer(
+        f'Ваш результат: {user_right_answers_percent}%\n'
+        f'Решено правильно: {user_statistics.right_answers_count}\n'
+        f'Всего вопросов: {user_statistics.all_questions}\n'
+    )
+    user_statistics.clear_stats()
     await state.clear()
     await message.answer('Возврат к выбору теста...',
                          reply_markup=keyboards.KEYBOARD_CHOOSING_TEST)
