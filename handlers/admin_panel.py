cf2bfa2f5c82192dee32fde42d6e27154d7e35db
MUstinov77 <m.ustinov2001@yandex.ru>
2025-09-06 06:47:54 +0300

    refactor admin_panel for new session factory

--- handlers/admin_panel.py
+++ handlers/admin_panel.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, F, Router
+from aiogram import F, Router
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, Message
