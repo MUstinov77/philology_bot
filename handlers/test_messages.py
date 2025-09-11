b6d024ab53b6d8b521fcb0d632a7f467d7850125
MUstinov77 <m.ustinov2001@yandex.ru>
2025-09-11 13:49:49 +0300

    dynamic keyboard for choosing test

--- handlers/test_messages.py
+++ handlers/test_messages.py
@@ -2,6 +2,7 @@ from aiogram import F, Router
 from aiogram.filters import StateFilter
 from aiogram.types import Message
 
+from db.tests import get_tests
 from keyboards import keyboards
 
 router = Router()
@@ -10,12 +11,19 @@ router = Router()
 @router.message(StateFilter(None), F.text.lower() == 'начать тест')
 async def chose_test(message: Message):
     # TODO: add fetching tests from db and refactor keybord for callbacks
-    await message.answer('Выберите тест:\n'
-                         '/accent_test - тест на постановку ударения в словах',
-                         reply_markup=keyboards.KEYBOARD_CHOOSING_TEST)
+    tests = get_tests()
+    tests_message = "Выберите тест: \n"
+    for test in tests:
+        tests_message += f"{test.id}. /{test.test_name}\n"
+    await message.answer(
+        tests_message,
+        reply_markup=keyboards.dynamic_keyboard_builder(len(tests)).as_markup(resize_keyboard=True)
+    )
 
 
 @router.message(StateFilter(None), F.text.lower() == 'отмена')
 async def proceed_answer(message: Message):
-    await message.answer('Вы прервали выбор теста',
-                         reply_markup=keyboards.KEYBOARD_START)
+    await message.answer(
+        'Вы прервали выбор теста',
+        reply_markup=keyboards.KEYBOARD_START
+    )
